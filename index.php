5014086e263a88117762e34aa2d75265a66c49c1
Maicon501 <156594791+Maicon501@users.noreply.github.com>
2024-02-28 23:08:35 -0300

    Update index.php

--- index.php
+++ index.php
@@ -2,13 +2,13 @@
 
 echo "Ola mundo 456";
 
-<<<<<<< HEAD
+
 echo "ola Udemy";
-=======
+
 echo "Ola Maicon";
 
 echo "Ola Amor Danielle";
 
->>>>>>> 6529d787be99f8cb7529a19662714ea769e18de7
+
 
 ?>
